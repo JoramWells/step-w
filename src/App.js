c43dde171f5b363a53b1e8b5f08d599d93ae414a
unknown <jorammanoah1@gmail.com>
2021-05-16 00:22:25 -0700

    Added antd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-    Hey
-    </div>
+    <>
+    <NavBar />
+    </>
   );
 }
 
